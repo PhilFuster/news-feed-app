64b38e396b4cefefd943ffe53ed0e45d22ec2866
PhilFuster <pafus23@protonmail.com>
2020-07-26 19:03:53 -0400

    Update on where the news feed app is right before gears change a bit. I understand that subscribing to a news feed is probably a little outside of the scoop of this book but I wish I could have set this up to actually read from a news feed or something. I believe that would be done with an RSS Feed kind of thing? I think I will expand on this at a later time, maybe connecting to a twitter feed and reading posts from someones handle. I think twitter is releasing a new API soon so I will maybe wait for that to come back here and set that up.

--- src/hooks/useJazzyNews.js
+++ src/hooks/useJazzyNews.js
@@ -1,34 +1,33 @@
-import { React, useState, useEffect } from 'react';
+import { React, useState, useEffect, useMemo } from 'react';
 import welcomeChime from '../../public/sounds/helloWindChimes.wav';
 import goodbyeChime from '../../public/sounds/goodbyeChimes.wav';
+import newPostChime from '../../public/sounds/newPostChime.wav';
 import newsFeed from '../../public/data/newsFeed-data';
 const useJazzyNews = () => {
-  const [posts, setPosts] = useState([]);
+  const [_posts, setPosts] = useState([]);
   const addPost = (post) => setPosts((allPosts) => [post, ...allPosts]);
 
-  /**
-   * a lot going on here. Let's break this into smaller useEffect functions.
-   */
-  // useEffect(() => {
-  //   NewsFeed.subscribe(addPost);
-  //   welcomeChime.play();
-  //   return () => {
-  //     NewsFeed.unsubscribe(addPost);
-  //     goodbyeChime.play();
-  //   };
-  // }, []);
+  const posts = useMemo(() => _posts, [_posts]);
 
+  useEffect(() => {
+    newPostChime.play();
+  }, [posts]);
+
+  // on component creation subscribe
+  // on tear down unsubscribe
   useEffect(() => {
     newsFeed.subscribe(addPost);
 
     return newsFeed.unsubscribe(addPost);
   }, []);
+  /**
+   * play chime on component creation
+   * and play goodbyeChime on tear down
+   */
   useEffect(() => {
     welcomeChime.play();
-
     return () => goodbyeChime.play();
-  });
-
+  }, []);
   return posts;
 };
 
